d4e3f9a9a27202fd5e144a448891e2e0ccfe9f0e
John Boehr <jbboehr@gmail.com>
2012-03-04 01:42:46 -0800

    working on c++ implementation

--- mustache.hpp
+++ mustache.hpp
@@ -10,13 +10,37 @@ extern "C" {
 #include <string>
 #include <map>
 #include <list>
+#include <stack>
 
 using namespace std;
+
+const int MUSTACHE_NODE_ROOT = 1;
+const int MUSTACHE_NODE_OUTPUT = 2;
+const int MUSTACHE_NODE_TAG = 3;
+
+const int MUSTACHE_FLAG_NONE = 0;
+const int MUSTACHE_FLAG_ESACPE = 1;
+const int MUSTACHE_FLAG_NEGATE = 2;
+const int MUSTACHE_FLAG_SECTION = 4;
+const int MUSTACHE_FLAG_STOP = 8;
+const int MUSTACHE_FLAG_COMMENT = 16;
+const int MUSTACHE_FLAG_PARTIAL = 32;
+const int MUSTACHE_FLAG_INLINE_PARTIAL = 64;
+
+const int MUSTACHE_CAN_HAVE_CHILDREN = MUSTACHE_FLAG_SECTION | MUSTACHE_FLAG_NEGATE | MUSTACHE_FLAG_INLINE_PARTIAL;
 
 class MustacheData {
   public:
     string val;
     list< map<string,MustacheData> > dat;
+};
+
+class MustacheNode {
+  public:
+    int type;
+    int flags;
+    string * data;
+    list<MustacheNode *> children;
 };
 
 class Mustache {
@@ -31,6 +55,7 @@ class Mustache {
     string getStartSequence();
     string getStopSequence();
     string * render(string * tmpl, map<string,MustacheData> * data);
+    MustacheNode * tokenize(string * tmpl);
 };
 
 
