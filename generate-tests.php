55aa5d4a866d7bc1b90767236c3576dd0f9115a7
John Boehr <jbboehr@gmail.com>
2012-03-28 15:55:34 -0700

    Converting line endings

--- generate-tests.php
+++ generate-tests.php
@@ -4,6 +4,7 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('memory_limit', '256M');
 ini_set('display_errors', true);
+define('MY_EOL', "\n"); // PHP_EOL
 
 // Argv
 if( !empty($argv[1]) && is_dir($argv[1]) ) {
@@ -11,7 +12,7 @@ if( !empty($argv[1]) && is_dir($argv[1]) ) {
 } else if( is_dir('./spec/specs') ) {
   $specDir = './spec/specs';
 } else {
-  echo 'Unable to find specs' . PHP_EOL;
+  echo 'Unable to find specs' . MY_EOL;
   exit(1);
 }
 
@@ -26,7 +27,7 @@ foreach( scandir($specDir) as $file ) {
 }
 
 if( empty($specs) ) {
-  echo 'No specs found in specified directory' . PHP_EOL;
+  echo 'No specs found in specified directory' . MY_EOL;
   exit(1);
 }
 
@@ -40,27 +41,27 @@ foreach( $specData as $spec => $data ) {
   $tests = $data['tests'];
   foreach( $tests as $test ) {
     $output = '';
-    $output .= '--TEST--' . PHP_EOL;
-    $output .= $test['name'] . PHP_EOL;
-    $output .= '--DESCRIPTION--' . PHP_EOL;
-    $output .= $test['desc'] . PHP_EOL;
-    $output .= '--SKIPIF--' . PHP_EOL;
-    $output .= "<?php if(!extension_loaded('mustache')) die('skip '); ?>" . PHP_EOL;
-    $output .= '--FILE--' . PHP_EOL;
-    $output .= '<?php' . PHP_EOL;
+    $output .= '--TEST--' . MY_EOL;
+    $output .= $test['name'] . MY_EOL;
+    $output .= '--DESCRIPTION--' . MY_EOL;
+    $output .= $test['desc'] . MY_EOL;
+    $output .= '--SKIPIF--' . MY_EOL;
+    $output .= "<?php if(!extension_loaded('mustache')) die('skip '); ?>" . MY_EOL;
+    $output .= '--FILE--' . MY_EOL;
+    $output .= '<?php' . MY_EOL;
     // MAIN
-    $output .= '$test = ' . var_export($test, true) . ';' . PHP_EOL;
-    $output .= '$mustache = new Mustache();' . PHP_EOL;
+    $output .= '$test = ' . var_export($test, true) . ';' . MY_EOL;
+    $output .= '$mustache = new Mustache();' . MY_EOL;
     if( !empty($test['partials']) && is_array($test['partials']) ) {
-      $output .= 'echo $mustache->render($test["template"], $test["data"], $test["partials"]);' . PHP_EOL;
+      $output .= 'echo $mustache->render($test["template"], $test["data"], $test["partials"]);' . MY_EOL;
     } else {
-      $output .= 'echo $mustache->render($test["template"], $test["data"]);' . PHP_EOL;
+      $output .= 'echo $mustache->render($test["template"], $test["data"]);' . MY_EOL;
     }
     // END MAIN
-    $output .= '?>' . PHP_EOL;
-//    $output .= '--EXPECT--' . PHP_EOL;
+    $output .= '?>' . MY_EOL;
+//    $output .= '--EXPECT--' . MY_EOL;
 //    $output .= $test['expected'];
-    $output .= '--EXPECTREGEX--' . PHP_EOL;
+    $output .= '--EXPECTREGEX--' . MY_EOL;
     $tmp = array();
     foreach( preg_split('/\s+/', $test['expected']) as $chunk ) {
       $tmp[] = preg_quote($chunk, '/');
@@ -69,8 +70,8 @@ foreach( $specData as $spec => $data ) {
     // Hack in XFAIL
     if( ($spec == 'partials' && $test['name'] == 'Standalone Line Endings') ||
         ($spec == 'partials' && $test['name'] == 'Standalone Without Previous Line') ) {
-      //$output .= PHP_EOL;
-      //$output .= '--XFAIL--' . PHP_EOL;
+      //$output .= MY_EOL;
+      //$output .= '--XFAIL--' . MY_EOL;
       //$output .= 'This extension does not follow the spec\'s whitespace rules.';
       $expected = "\s*" . join("\s*", str_split(preg_replace('/\s+/', '', $test['expected']), 1)) . "\s*";
     }
