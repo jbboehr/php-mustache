53dcbac322d20d41af38f3197923d3acbcf2384b
John Boehr <john@johnboehr.is>
2018-11-10 21:17:48 -0800

    Change default.nix to use the local path by default

--- default.nix
+++ default.nix
@@ -3,24 +3,18 @@
   php ? pkgs.php,
 
   phpMustacheVersion ? null,
-  phpMustacheSrc ? null,
+  phpMustacheSrc ? ./.,
   phpMustacheSha256 ? null,
 
-  mustacheSpecVersion ? null,
-  mustacheSpecSrc ? null,
-  mustacheSpecSha256 ? null,
   mustache_spec ? pkgs.callPackage (import ((fetchTarball {
-    url = https://github.com/jbboehr/mustache-spec/archive/210109d9ccd05171233d8d7a8ceb97f3bddc790a.tar.gz;
-    sha256 = "0ss51lraznpbixahfdj7j7wg41l1gnwq9qbm9aw2lkc9vvsc3h3c";
-  }) + "/derivation.nix")) { inherit mustacheSpecVersion mustacheSpecSrc mustacheSpecSha256; },
+    url = https://github.com/jbboehr/mustache-spec/archive/5b85c1b58309e241a6f7c09fa57bd1c7b16fa9be.tar.gz;
+    sha256 = "1h9zsnj4h8qdnzji5l9f9zmdy1nyxnf8by9869plyn7qlk71gdyv";
+  }))) {},
 
-  libmustacheVersion ? null,
-  libmustacheSrc ? null,
-  libmustacheSha256 ? null,
   libmustache ? pkgs.callPackage (import ((fetchTarball {
-    url = https://github.com/jbboehr/libmustache/archive/ddf7eb217e04fc42624911913f30009739c9ead4.tar.gz;
-    sha256 = "19bn5kgihf5b2y6lpp259ar616j2mznskzmh57icdyr6wpk2zw90";
-  }) + "/derivation.nix")) { inherit libmustacheVersion libmustacheSrc libmustacheSha256 mustache_spec; }
+    url = https://github.com/jbboehr/libmustache/archive/bebc4573a849fcfb9054462752b75bdb181d9607.tar.gz;
+    sha256 = "1zngfl794vrchxwz5qs80j5421dhqxsmr0lnz1cwxjv06s8zv8iz";
+  }))) { inherit mustache_spec; }
 }:
 
 pkgs.callPackage ./derivation.nix {
