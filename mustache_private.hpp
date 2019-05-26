5051fa93e29f286e62d657499ee5acccd01d34f8
John Boehr <john@johnboehr.is>
2019-05-25 18:54:08 -0700

    Work around PHP8 compat issue
    See https://github.com/php/php-src/commit/43a7d95016761787cace63fb52e93e27e123d0cc

--- mustache_private.hpp
+++ mustache_private.hpp
@@ -11,5 +11,10 @@
 
 #include "mustache/mustache.hpp"
 
+// Related?: https://github.com/php/php-src/commit/43a7d95016761787cace63fb52e93e27e123d0cc
+#ifndef ZEND_ACC_CTOR
+#define ZEND_ACC_CTOR 0
+#endif
+
 #endif /* PHP_MUSTACHE_PRIVATE_HPP */
 
